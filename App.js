94d17db29620ad6bc59a0d08eadbb1082e1febe6
TaeGyun12 <dirori730@gmail.com>
2021-02-24 11:10:34 +0900

    First Commit (1) Weather 코드 추가

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import { render } from 'react-dom';
+import { Alert } from 'react-native';
+import axios from "axios";
+import Weather from "./weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "13f52a68fad52ea24a83e199bbb5248f";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getWeather = async(latitude, longitude) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+    const {data} = await axios.get(url);
+    console.log(data);
+    this.setState({isLoading: false, temp: data.main.temp});
+  }
+  getLocation = async() =>{
+    try{
+      await Location.requestPermissionsAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
+    }
+    catch (error){
+      Alert.alert("Can't Find You.")
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading, temp} = this.state;
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)}/>;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
